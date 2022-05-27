d88362c3b85a61b38ba7acf403c7f4f906cd2988
Vijayabalan Balakrishnan <bvijaycom@users.noreply.github.com>
2022-05-27 18:54:18 +0200

    Update docker-example.sh

--- Chapter01/docker-example.sh
+++ Chapter01/docker-example.sh
@@ -1,8 +1,8 @@
 #First we will pull an image
-docker pull docker/whalesay
+docker pull cloudnloud/azurek8s
 
 #We can then look at which images we have locally
 docker images
 
 #Then we will run our container
-docker run docker/whalesay cowsay boo
+docker run cloudnloud/azurek8s cowsay boo
